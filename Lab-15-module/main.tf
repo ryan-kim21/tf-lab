c694ac37a97a88c786848fe11701b1c26bc9883e
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-06-08 23:09:42 +0900

    2

--- Lab-15-module/main.tf
+++ Lab-15-module/main.tf
@@ -2,3 +2,11 @@ provider "aws" {
     region = ap-northeast-2
   
 }
+
+resource "aws_vpc" "main"{
+    cidr_block = var.vpc_cidr
+    tags = {
+        Name = "${var.env}-vpc"
+        Owner = "Ryan Kim"
+    }
+}
