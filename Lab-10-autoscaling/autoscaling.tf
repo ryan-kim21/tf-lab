1432fb14992ae94db60b1edafb31cb766da30554
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-06-12 23:49:36 +0900

    f

--- Lab-10-autoscaling/autoscaling.tf
+++ Lab-10-autoscaling/autoscaling.tf
@@ -4,7 +4,9 @@ resource "aws_launch_template" "web" {
   image_id               = data.aws_ami.latest_amazon_linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web.id]
-  user_data = filebase64("./user_data.sh")
+  user_data              = templatefile("${path.module}/user_data.sh", {})
+
+  # 추가 설정 및 속성
 }
 
 resource "aws_autoscaling_group" "web" {
