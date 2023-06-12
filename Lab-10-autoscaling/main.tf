bc40c3c769e56860f807d3bf8f3da94dc193c209
ryan-kim21 <63631441+ryan-kim21@users.noreply.github.com>
2023-06-12 23:21:29 +0900

    sds

--- Lab-10-autoscaling/main.tf
+++ Lab-10-autoscaling/main.tf
@@ -1,162 +1,141 @@
-
-# # Create: 
-# - Security Group for web server and Elb
-# - Launch Configuration with Auto AMI Lookup
-# - Auto Scaling Group using 2 Availablity Zones
-# - Classic load Balancer in 2 Availability Zones 
-# Update to Web server will be via Green/Blue Deployment Strategy
-# Developed by Ryan.
-
+#------------------------------------------------------------------
+#  Terraform - From Zero to Certified Professional
+#
+# Provision Highly Availabe Web Cluster in any Region Default VPC
+# Create:
+#    - Security Group for Web Server and ALB
+#    - Launch Template with Auto AMI Lookup
+#    - Auto Scaling Group using 2 Availability Zones
+#    - Application Load Balancer in 2 Availability Zones
+#    - Application Load Balancer TargetGroup
+# Update to Web Servers will be via Green/Blue Deployment Strategy
+# Developed by Denis Astahov
+#------------------------------------------------------------------
 provider "aws" {
-    region = "ap-northeast-2"
-  }
+  region = "us-west-1"
 
-data "aws_availability_zones" "working"{}
-
-
-data "aws_ami" "latest_amazon_linux"{
-    owners = ["960524191939"]
-    most_recent = true
-    filter {
-        name = "name"
-        values = ["amzn2-ami-hvm-*-x86-gp2"]
-    }
-}
-
-
-resource "aws_security_group" "web" {
-    name = "Web Security Group"
-    dynamic "ingress" {
-        for_each = ["80", "443"]
-        content {
-            from_port = ingress.value
-            to_port = ingress.value
-            protocol = "tcp"
-            cidr_blocks = ["0.0.0.0/0"]
-        }
-      
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    tags ={
-        Name = "Web Security Group"
-        Owner = "Ryan Kim"
-    }
-  
-}
-
-
-resource "aws_launch_configuration" "web" {
-    name_prefix = "WebServer-Highly-Available-LC-"
-    image_id = data.aws_ami.latest_amazon_linux.id #위 데이터 
-    instance_type = "t3.micro"
-    security_group_names = [aws_security_group.web.id]
-    user_data = file("user_data.sh")
-
-    lifecycle{
-        create_before_destroy = true
-    }
-
-}
-
-
-resource "aws_autoscaling_group" "web"{
-    name = "ASG-${aws_launch_configuration.web.name}" #launch_configration_name
-    launch_configuration = aws_launch_configuration.web.name
-    min_size = 1
-    max_size = 3
-    min_elb_capacity = 3
-    health_check_type = "ELB"
-    vpc_zone_identifier = ["aws_default_subnet.default_az1.id","aws_default_subnet.default_az2.id"]
-    load_balancers = [aws_elb.web.name] # lb name을 가져올 수 있음
-
-    dynamic "tag" {
-        for_each = {
-          Name = "WebServer in ASG"
-          Owner = "Ryan Kim"
-          TAGKEY = "TAGVALUE" 
-        }
-
-        content {
-          key = tag.key
-          value = tag.value
-          propogate_at_launch =true #?
-
-        }
-    }
-    lifecycle{
-        create_before_destroy = true
-    }
-
-}
-
-
-
-
-resource "aws_elb" "web" {
-    name = "WebServer-HighlyAvailable-ELB"
-    availability_zones = [data.aws_availability_zones.working.names[0], data.data.aws_availability_zones.working.names[1]]
-    security_groups = [aws_security_group.web.id]
-    listener{
-        lb_port = 80
-        lb_protocol = "http"
-        instance_port = 80
-        instance_protocol = "http"
-    }
-
-    health_check {
-      healthy_threshold = 2
-      unhealthy_threshold = 2
-      timeout = 3
-      target = "HTTP:80/"
-      interval = 10
-    }
+  default_tags {
     tags = {
-        Name = "WebServer-HighlyAvailable-ELB"
-        Owner = "Ryan Kim"
+      Owner     = "Denis Astahov"
+      CreatedBy = "Terraform"
+      Course    = "From Zero to Certified Professional"
     }
+  }
 }
 
+
+data "aws_availability_zones" "working" {}
+data "aws_ami" "latest_amazon_linux" {
+  owners      = ["960524191939"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+#-------------------------------------------------------------------------------
+resource "aws_default_vpc" "default" {}
 
 resource "aws_default_subnet" "default_az1" {
-    availability_zone = data.aws_availability_zones.working.names[0]
-  
+  availability_zone = data.aws_availability_zones.working.names[0]
 }
 
 resource "aws_default_subnet" "default_az2" {
-    availability_zone = data.aws_availability_zones.working.names[1]
-  
+  availability_zone = data.aws_availability_zones.working.names[1]
 }
 
+#-------------------------------------------------------------------------------
+resource "aws_security_group" "web" {
+  name   = "Web Security Group"
+  vpc_id = aws_default_vpc.default.id
+  dynamic "ingress" {
+    for_each = ["80", "443"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "Web Security Group"
+  }
+}
 
+#-------------------------------------------------------------------------------
+resource "aws_launch_template" "web" {
+  name                   = "WebServer-Highly-Available-LT"
+  image_id               = data.aws_ami.latest_amazon_linux.id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web.id]
+  user_data              = filebase64("${path.module}/user_data.sh")
+}
+
+resource "aws_autoscaling_group" "web" {
+  name                = "WebServer-Highly-Available-ASG-Ver-${aws_launch_template.web.latest_version}"
+  min_size            = 2
+  max_size            = 2
+  min_elb_capacity    = 2
+  health_check_type   = "ELB"
+  vpc_zone_identifier = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+  target_group_arns   = [aws_lb_target_group.web.arn]
+
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = aws_launch_template.web.latest_version
+  }
+
+  dynamic "tag" {
+    for_each = {
+      Name   = "WebServer in ASG-v${aws_launch_template.web.latest_version}"
+      TAGKEY = "TAGVALUE"
+    }
+    content {
+      key                 = tag.key
+      value               = tag.value
+      propagate_at_launch = true
+    }
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#-------------------------------------------------------------------------------
+resource "aws_lb" "web" {
+  name               = "WebServer-HighlyAvailable-ALB"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.web.id]
+  subnets            = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
+}
+
+resource "aws_lb_target_group" "web" {
+  name                 = "WebServer-HighlyAvailable-TG"
+  vpc_id               = aws_default_vpc.default.id
+  port                 = 80
+  protocol             = "HTTP"
+  deregistration_delay = 10 # seconds
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.web.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web.arn
+  }
+}
+
+#-------------------------------------------------------------------------------
 output "web_loadbalancer_url" {
-    value = aws_elb.web.dns_name
-  
+  value = aws_lb.web.dns_name
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
